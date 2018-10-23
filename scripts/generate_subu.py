030c2a3ee3a0217ba0f5bf4fcbd2bec9717a96e6
Sharad Vikram <sharad.vikram@gmail.com>
2018-10-22 22:41:32 -0700

    loadded models from s3

--- scripts/generate_subu.py
+++ scripts/generate_subu.py
@@ -1,3 +1,4 @@
+import s3fs
 import tqdm
 from argparse import ArgumentParser
 import numpy as np
@@ -16,8 +17,11 @@ from metasense.data import load
 X_features = ['no2', 'o3', 'co', 'temperature', 'absolute-humidity', 'pressure']
 Y_features = ['epa-no2', 'epa-o3']
 
+BUCKET_NAME = "metasense-paper-results"
+
 def parse_args():
     argparser = ArgumentParser()
+    argparser.add_argument('experiment')
     argparser.add_argument('name')
     argparser.add_argument('--level0', action='store_true')
     argparser.add_argument('--level1', action='store_true')
@@ -47,9 +51,9 @@ def get_triples():
             for board in DATA[round][location]:
                 yield (round, location, board)
 
-def level0(out_dir):
+def level0(out_dir, experiment_dir):
 
-    model_dir = out_dir / 'level1' / 'models'
+    model_dir = experiment_dir / 'level1' / 'models'
     (out_dir / 'level0').mkdir(exist_ok=True)
 
     differences = pd.DataFrame(columns=[
@@ -61,8 +65,9 @@ def level0(out_dir):
     test_results = pd.DataFrame(columns=[
         'Model', 'Testing Location', 'NO2 MAE', 'O3 MAE', 'NO2 CvMAE', 'O3 CvMAE'
     ])
-    for file in tqdm.tqdm(list(model_dir.glob('*'))):
-        triple, model = joblib.load(file)
+    for file in tqdm.tqdm(list(fs.glob(str(model_dir / '*')))):
+        with fs.open(file, 'rb') as fp:
+            triple, model = joblib.load(fp)
         train_result = benchmark(model, triple, train=True)
         test_result = benchmark(model, triple, train=False)
         difference = train_result - test_result
@@ -104,10 +109,10 @@ def level0(out_dir):
         fp.write(differences.sort_values(['Model', 'Testing Location']).to_latex())
 
 
-def level1(out_dir):
+def level1(out_dir, experiment_dir):
     RESULTS = {}
 
-    model_dir = out_dir / 'level1' / 'models'
+    model_dir = experiment_dir / 'level1' / 'models'
     (out_dir / 'level1').mkdir(exist_ok=True)
 
     for round, location, board in get_triples():
@@ -130,8 +135,9 @@ def level1(out_dir):
     test_results = pd.DataFrame(columns=[
         'Model', 'Testing Location', 'NO2 MAE', 'O3 MAE', 'NO2 CvMAE', 'O3 CvMAE'
     ])
-    for file in tqdm.tqdm(list(model_dir.glob('*'))):
-        triple, model = joblib.load(file)
+    for file in tqdm.tqdm(list(fs.glob(str(model_dir / '*')))):
+        with fs.open(file, 'rb') as fp:
+            triple, model = joblib.load(fp)
         tests = RESULTS[triple]
         train_result = benchmark(model, triple)
         if len(tests) > 0:
@@ -175,9 +181,9 @@ def level1(out_dir):
     with open(str(out_dir / 'level1' / 'difference.tex'), 'w') as fp:
         fp.write(differences.sort_values(['Model', 'Testing Location']).to_latex())
 
-def level2(out_dir):
+def level2(out_dir, experiment_dir):
 
-    model_dir = out_dir / 'level2' / 'models'
+    model_dir = experiment_dir / 'level2' / 'models'
     (out_dir / 'level2').mkdir(exist_ok=True)
 
     boards = {}
@@ -197,9 +203,10 @@ def level2(out_dir):
     test_results = pd.DataFrame(columns=[
         'Model', 'NO2 MAE', 'O3 MAE', 'NO2 CvMAE', 'O3 CvMAE'
     ])
-    for model_file in tqdm.tqdm(list(model_dir.glob('*'))):
+    for model_file in tqdm.tqdm(list(fs.glob(str(model_dir / '*')))):
         print("Loading model...")
-        (board_id, train_config), model = joblib.load(model_file)
+        with fs.open(model_file, 'rb') as fp:
+            (board_id, train_config), model = joblib.load(fp)
         test_config = list(boards[board_id] - train_config)[0]
         print("Benchmarking train...")
         train_result = benchmark(model, [t + (board_id,) for t in train_config])
@@ -309,8 +316,8 @@ def level25(out_dir):
     with open(str(out_dir / 'level2.5' / 'difference.tex'), 'w') as fp:
         fp.write(differences.to_latex())
 
-def level3(out_dir, seed):
-    model_dir = out_dir / 'level3' / 'models'
+def level3(out_dir, experiment_dir, seed):
+    model_dir = experiment_dir / 'level3' / 'models'
     (out_dir / 'level3').mkdir(exist_ok=True)
 
     boards = {}
@@ -330,8 +337,9 @@ def level3(out_dir, seed):
     test_results = pd.DataFrame(columns=[
         'Model', 'Test', 'NO2 MAE', 'O3 MAE', 'NO2 CvMAE', 'O3 CvMAE'
     ])
-    for model_file in tqdm.tqdm(list(model_dir.glob('*'))):
-        board_id, model = joblib.load(model_file)
+    for model_file in tqdm.tqdm(list(fs.glob(str(model_dir / '*')))):
+        with fs.open(model_file, 'rb') as fp:
+            board_id, model = joblib.load(fp)
         for t in boards[board_id]:
             train_data, test_data = load(*(t[0], t[1], board_id))
             # train_data = pd.concat([t[0] for t in data])
@@ -399,8 +407,9 @@ def level4(out_dir, seed):
         'Model', 'Test', 'NO2 MAE', 'O3 MAE', 'NO2 CvMAE', 'O3 CvMAE'
     ])
     models = {}
-    for model_file in tqdm.tqdm(list(model_dir.glob('*'))):
-        board_id, model = joblib.load(model_file)
+    for model_file in tqdm.tqdm(list(fs.glob(str(model_dir / '*')))):
+        with fs.open(model_file, 'rb') as fp:
+            board_id, model = joblib.load(fp)
         if board_id[2] not in models:
             models[board_id[2]] = []
         models[board_id[2]].append(model)
@@ -449,18 +458,21 @@ def level4(out_dir, seed):
 
 if __name__ == "__main__":
     args = parse_args()
-    out_dir = Path('results') / args.name
+    out_dir = Path('results') / args.experiment/ args.name
     out_dir.mkdir(parents=True, exist_ok=True)
 
+    fs = s3fs.S3FileSystem(anon=False)
+    experiment_dir = Path(BUCKET_NAME) / args.experiment / args.name
+
     if args.level0:
-        level0(out_dir)
+        level0(out_dir, experiment_dir)
     if args.level1:
-        level1(out_dir)
+        level1(out_dir, experiment_dir)
     if args.level2:
-        level2(out_dir)
+        level2(out_dir, experiment_dir)
     if args.level25:
-        level25(out_dir)
+        level25(out_dir, experiment_dir)
     if args.level3:
-        level3(out_dir, args.seed)
+        level3(out_dir, experiment_dir, args.seed)
     if args.level4:
-        level4(out_dir, args.seed)
+        level4(out_dir, experiment_dir, args.seed)
