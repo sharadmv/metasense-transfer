816ed028b1f767bb53cfcd3b08a2a36ed1caf1d7
Sharad Vikram <sharad.vikram@gmail.com>
2018-10-28 08:30:06 +0000

    updated big script

--- scripts/train_splits.sh
+++ scripts/train_splits.sh
@@ -1,30 +1,30 @@
 set -x
-python scripts/train_split_model.py splits-100-3 4_elcajon_11 --round 4  --location elcajon --board 11
-python scripts/train_split_model.py splits-100-3 4_elcajon_12 --round 4  --location elcajon --board 12
-python scripts/train_split_model.py splits-100-3 4_elcajon_13 --round 4  --location elcajon --board 13
-python scripts/train_split_model.py splits-100-3 4_donovan_17 --round 4  --location donovan --board 17
-python scripts/train_split_model.py splits-100-3 4_donovan_19 --round 4  --location donovan --board 19
-python scripts/train_split_model.py splits-100-3 4_donovan_21 --round 4  --location donovan --board 21
-python scripts/train_split_model.py splits-100-3 4_shafter_15 --round 4  --location shafter --board 15
-python scripts/train_split_model.py splits-100-3 4_shafter_18 --round 4  --location shafter --board 18
-python scripts/train_split_model.py splits-100-3 4_shafter_20 --round 4  --location shafter --board 20
+python scripts/train_split_model.py splits-500-100 4_elcajon_11 --round 4  --location elcajon --board 11 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_elcajon_12 --round 4  --location elcajon --board 12 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_elcajon_13 --round 4  --location elcajon --board 13 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_donovan_17 --round 4  --location donovan --board 17 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_donovan_19 --round 4  --location donovan --board 19 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_donovan_21 --round 4  --location donovan --board 21 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_shafter_15 --round 4  --location shafter --board 15 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_shafter_18 --round 4  --location shafter --board 18 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 4_shafter_20 --round 4  --location shafter --board 20 --dim 100 --hidden_size 500
 
-python scripts/train_split_model.py splits-100-3 2_elcajon_17 --round 2  --location elcajon --board 17
-python scripts/train_split_model.py splits-100-3 2_elcajon_19 --round 2  --location elcajon --board 19
-python scripts/train_split_model.py splits-100-3 2_elcajon_21 --round 2  --location elcajon --board 21
-python scripts/train_split_model.py splits-100-3 2_donovan_15 --round 2  --location donovan --board 15
-python scripts/train_split_model.py splits-100-3 2_donovan_18 --round 2  --location donovan --board 18
-python scripts/train_split_model.py splits-100-3 2_donovan_20 --round 2  --location donovan --board 20
-python scripts/train_split_model.py splits-100-3 2_shafter_11 --round 2  --location shafter --board 11
-python scripts/train_split_model.py splits-100-3 2_shafter_12 --round 2  --location shafter --board 12
-python scripts/train_split_model.py splits-100-3 2_shafter_13 --round 2  --location shafter --board 13
+python scripts/train_split_model.py splits-500-100 2_elcajon_17 --round 2  --location elcajon --board 17 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_elcajon_19 --round 2  --location elcajon --board 19 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_elcajon_21 --round 2  --location elcajon --board 21 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_donovan_15 --round 2  --location donovan --board 15 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_donovan_18 --round 2  --location donovan --board 18 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_donovan_20 --round 2  --location donovan --board 20 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_shafter_11 --round 2  --location shafter --board 11 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_shafter_12 --round 2  --location shafter --board 12 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 2_shafter_13 --round 2  --location shafter --board 13 --dim 100 --hidden_size 500
 
-python scripts/train_split_model.py splits-100-3 3_elcajon_15 --round 3  --location elcajon --board 15
-python scripts/train_split_model.py splits-100-3 3_elcajon_18 --round 3  --location elcajon --board 18
-python scripts/train_split_model.py splits-100-3 3_elcajon_20 --round 3  --location elcajon --board 20
-python scripts/train_split_model.py splits-100-3 3_donovan_11 --round 3  --location donovan --board 11
-python scripts/train_split_model.py splits-100-3 3_donovan_12 --round 3  --location donovan --board 12
-python scripts/train_split_model.py splits-100-3 3_donovan_13 --round 3  --location donovan --board 13
-python scripts/train_split_model.py splits-100-3 3_shafter_17 --round 3  --location shafter --board 17
-python scripts/train_split_model.py splits-100-3 3_shafter_19 --round 3  --location shafter --board 19
-python scripts/train_split_model.py splits-100-3 3_shafter_21 --round 3  --location shafter --board 21
+python scripts/train_split_model.py splits-500-100 3_elcajon_15 --round 3  --location elcajon --board 15 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_elcajon_18 --round 3  --location elcajon --board 18 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_elcajon_20 --round 3  --location elcajon --board 20 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_donovan_11 --round 3  --location donovan --board 11 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_donovan_12 --round 3  --location donovan --board 12 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_donovan_13 --round 3  --location donovan --board 13 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_shafter_17 --round 3  --location shafter --board 17 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_shafter_19 --round 3  --location shafter --board 19 --dim 100 --hidden_size 500
+python scripts/train_split_model.py splits-500-100 3_shafter_21 --round 3  --location shafter --board 21 --dim 100 --hidden_size 500
