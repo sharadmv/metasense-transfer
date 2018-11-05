4065142d914ca2227534606ffb7f3a629404a79b
Sharad Vikram <sharad.vikram@gmail.com>
2018-11-04 16:49:27 -0800

    updated metrics

--- metasense/models/model.py
+++ metasense/models/model.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 class Model(object, metaclass=ABCMeta):
 
@@ -18,6 +18,22 @@ class Model(object, metaclass=ABCMeta):
     def score(self, *args):
         y = args[-1]
         preds = self.predict(*args[:-1])
+        y = y.as_matrix()
+        mean_y = y.mean(axis=0)
+        mean_pred = preds.mean(axis=0)
+        crmse = np.sqrt(np.square((preds - mean_pred[None]) - (y - mean_y[None])).mean(axis=0))
         mae = mean_absolute_error(y, preds, multioutput='raw_values')
-        cvmae = np.array(mae / y.mean())
-        return mae, cvmae
+        mse = mean_squared_error(y, preds, multioutput='raw_values')
+        r2 = r2_score(y, preds, multioutput='raw_values')
+        rmse = np.sqrt(mse)
+        mbe = (y - preds).mean(axis=0)
+        cvmae = np.array(mae / y.mean(axis=0))
+        return {
+            'MAE': mae,
+            'CvMAE': cvmae,
+            'MBE': mbe,
+            'MSE': mse,
+            'rMSE': rmse,
+            'R^2': r2,
+            'crMSE': crmse
+        }, preds
